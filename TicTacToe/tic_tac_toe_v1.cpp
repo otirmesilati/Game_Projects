b35990dddcafc7c21602e4d436d5f01bf0950970
Otyr <otirmesi@gmail.com>
2024-11-14 09:50:19 +0200

    Assets

--- TicTacToe/tic_tac_toe_v1.cpp
+++ TicTacToe/tic_tac_toe_v1.cpp
@@ -1,47 +1,56 @@
-// version 1: output UI not the best
-
 #pragma once
 #include <iostream>
+#define BOARD_LENGTH 3
+#define BOARD_WIDTH 3
+
 using namespace std;
 
-/*
-TODO:
+// TODO: 1's done, remaining are 2, 3 and 4
 
-Check for Win/Draw: Create functions to check if a player has won or if the game is a draw.
-Game Loop: Implement the main game loop to alternate turns between players.
 
-*/
+void show_board(char game_board[BOARD_LENGTH][BOARD_WIDTH])
+{
+    for(int board_row_index = 0; board_row_index < BOARD_LENGTH;)
+    {
+        for(int board_column_index = 0; board_column_index < BOARD_WIDTH ; ++board_column_index){
+            cout << game_board[board_column_index][board_row_index];
+        }
+    }
+}
 
-void show_board();
+bool win_condition_check(char game_board[BOARD_LENGTH][BOARD_WIDTH])
+{
+    // a for loop for checking the lines
+    for(int linex_index = 0; linex_index < BOARD_LENGTH; ++linex_index)
+    {
+        if(game_board[linex_index][0] == game_board[linex_index][1] == game_board[linex_index][2]) 
+        return true;
+    }
 
-char game_board[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-char current_player = 'X';
+    // a for loop for checking the rows
+    for(int row_index = 0; row_index < BOARD_WIDTH; ++row_index) 
+    {
+        if(game_board[0][row_index] == game_board[1][row_index] == game_board[2][row_index]) // finish this
+        return true;
+    }
+
+    // a for loop for checking the diagonals
+    for(int diagonal_index = 0; diagonal_index < BOARD_LENGTH; ++diagonal_index)
+    {
+        
+    } 
+
+    return false; 
+}
+
+// 2. a function for setting the board
+
+// 3. a function for checking if the winner won
+
+// 4. a function for playing the game
 
 int main()
 {
-    int x_pos = NULL, y_pos = NULL;
-
-    show_board();
-    cout << "Player " << current_player << " enter your position" << endl;
-
+    // call game function
     return NULL;
-}
-
-void show_board()
-{
-    cout << "   0 1 2 \n";
-    for(int i = 0; i < 3; ++i)
-    {
-        cout << i << " ";
-        for(int j = 0; j < 3; ++j)
-        {
-            cout << game_board[i][j];
-        }
-
-        cout << endl;
-    }
-
-    cout << endl;
-
-    return;
 }
