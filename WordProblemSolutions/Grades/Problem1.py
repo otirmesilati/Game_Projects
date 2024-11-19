1e5ab9f6476afcdfd26c6ae8751a9e6a6dc6a257
Otyr <otirmesi@gmail.com>
2024-11-19 15:49:10 +0200

    Assets

---
+++ WordProblemSolutions/Grades/Problem1.py
@@ -0,0 +1,10 @@
+
+def main():
+
+    student_grade = input("What grade has the student got?")
+
+    student_grade += 5
+    print(f"After a thorough check by the teacher, she decided to factor by 5 points, the new grade is: {student_grade}")
+
+if __name__ == '__main__':
+    main()
