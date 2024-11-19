4e0c810f278dc582a040e2a4bb5825839683ac3a
Otyr <otirmesi@gmail.com>
2024-11-19 10:20:28 +0200

    Assets

--- WordProblemSolutions/Grades/Problem1.ml
+++ WordProblemSolutions/Grades/Problem1.ml
@@ -2,4 +2,4 @@ let main () =
 print_endline "What grade has the student got?";
 let student_grade = read_int () in
 let new_grade = student_grade + 5 in
-(* do the formatting *)
+(* do the formatting here *)
