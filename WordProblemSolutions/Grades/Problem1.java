0ba2f09bce4fc5f898721c604066b65a74c3c1bf
Otyr <otirmesi@gmail.com>
2024-11-19 09:59:51 +0200

    Assets

---
+++ WordProblemSolutions/Grades/Problem1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Problem1
+{
+    public static Scanner reader = new Scanner(System.in);
+
+    public static void main(String[]args)
+    {
+        int student_grade = 0;
+        System.out.println("What grade has the student got?");
+
+        student_grade +=5;
+        System.out.println(String.format("After a thorough check by the teacher, she decided to factor by 5 points, the new grade is: %d", student_grade);
+    }
+
+}
---
+++ WordProblemSolutions/Grades/Problem1.ml
@@ -0,0 +1,5 @@
+let main () =
+print_endline "What grade has the student got?";
+let student_grade = read_int () in
+let new_grade = student_grade + 5 in
+(* do the formatting *)
